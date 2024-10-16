da95d12856e71e190955479350a4e4bf21bfba38
MRBOX <olamigokeadebayo90@gmail.com>
2024-10-16 22:37:23 +0100

    Background Color#

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,9 +1,37 @@
 import React from 'react'
+import Logo from "../assets/Images/logo.png"
 
 const Header = () => {
+    const menu = [
+        {
+            id:1,
+            name:"Home"
+        },
+        {
+            id:2,
+            name:"Movies"
+        },
+        {
+            id:3,
+            name:"TV"
+        },
+        {
+            id:4,
+            name:"Sports"
+        },
+        {
+            id:5,
+            name:"MyStuff"
+        },
+        {
+            id:6,
+            name:"Hub"
+        },
+
+    ]
   return (
     <div>
-      
+      <img src={Logo } alt=""/>
     </div>
   )
 }
